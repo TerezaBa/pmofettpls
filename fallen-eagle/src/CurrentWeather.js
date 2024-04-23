201c4c47d316cf509c3322a5b75d2e0435b15897
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2024-04-23 16:09:56 +0200

    Added useEffect for fetching data in DataSearch.js

--- fallen-eagle/src/CurrentWeather.js
+++ fallen-eagle/src/CurrentWeather.js
@@ -1,69 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import Temperature from "./Temperature";
-// import moment from "moment-timezone";
-
-// import "./styles/CurrentWeather.css";
-
-// export default function CurrentWeather(props) {
-//   const [searched, setSearched] = useState(false);
-//   const [currentTime, setCurrentTime] = useState(null);
-
-//   useEffect(() => {
-//     setSearched(false);
-//     setCurrentTime(null);
-//     search();
-//   }, [props.weatherInfo.coords]);
-
-//   function showTime(response) {
-//     let localTime = response.data.formatted;
-//     setCurrentTime(localTime);
-//     setSearched(true);
-//   }
-
-//   function search() {
-//     let lat = props.weatherInfo.coords.latitude;
-//     let lon = props.weatherInfo.coords.longitude;
-//     let apiKey = "OA2X7WH81GI9";
-//     let apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
-//     axios.get(apiUrl).then(showTime);
-//   }
-
-//   if (searched) {
-//     return (
-//       <div className="row CurrentWeather">
-//         <div className="col-6">
-//           <h1>{props.weatherInfo.city}</h1>
-//           <h6>
-//             {currentTime
-//               ? moment(currentTime).format("ddd Do MMMM, hh:mm A")
-//               : "Loading..."}
-//           </h6>
-//           <h4 className="text-capitalize">{props.weatherInfo.desc}</h4>
-//         </div>
-//         <div className="col-6 align-self-center">
-//           <Temperature
-//             metric={props.weatherInfo.temp}
-//             icon={props.weatherInfo.icon}
-//           />
-//           <ul>
-//             <li>
-//               <span className="info-text">Humidity:</span>{" "}
-//               {props.weatherInfo.hum}%
-//             </li>
-//             <li>
-//               <span className="info-text">Wind:</span> {props.weatherInfo.wind}{" "}
-//               m/s
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   } else {
-//     // search();
-//     return null;
-//   }
-// }
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Temperature from "./Temperature";
@@ -72,40 +6,35 @@ import moment from "moment-timezone";
 import "./styles/CurrentWeather.css";
 
 export default function CurrentWeather(props) {
-  const [searched, setSearched] = useState(false);
   const [currentTime, setCurrentTime] = useState(null);
 
   useEffect(() => {
-    const search = () => {
+    // setSearched(false);
+    setCurrentTime(null);
+    async function search() {
       let lat = props.weatherInfo.coords.latitude;
       let lon = props.weatherInfo.coords.longitude;
       let apiKey = "OA2X7WH81GI9";
       let apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
-
-      axios
-        .get(apiUrl)
-        .then((response) => {
-          let localTime = response.data.formatted;
-          setCurrentTime(localTime);
-          setSearched(true);
-        })
-        .catch((error) => {
-          console.error("Error fetching timezone data:", error);
-          // Optionally set an error state and show it in the UI
-          setSearched(true); // Ensure that we stop loading even on error
-        });
-    };
-
-    // Check if coordinates are available before calling search
-    if (props.weatherInfo.coords) {
-      setSearched(false); // Reset searched to trigger the loading state
-      setCurrentTime(null); // Clear current time while loading new data
-      search();
+      const response = await axios.get(apiUrl);
+      showTime(response);
     }
-  }, [props.weatherInfo.coords]); // Effect dependencies should include all values from the prop that are used in the effect.
+    search();
+  }, [props.weatherInfo.coords]);
 
-  if (!searched) {
-    return <div>Loading...</div>; // Show a loading indicator when data is being fetched
+  function showTime(response) {
+    let localTime = response.data.formatted;
+    setCurrentTime(localTime);
+    // setSearched(true);
+  }
+
+  async function search() {
+    let lat = props.weatherInfo.coords.latitude;
+    let lon = props.weatherInfo.coords.longitude;
+    let apiKey = "OA2X7WH81GI9";
+    let apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
+    const response = await axios.get(apiUrl);
+    showTime(response);
   }
 
   return (
@@ -115,7 +44,7 @@ export default function CurrentWeather(props) {
         <h6>
           {currentTime
             ? moment(currentTime).format("ddd Do MMMM, hh:mm A")
-            : "Error loading time data"}
+            : "Loading..."}
         </h6>
         <h4 className="text-capitalize">{props.weatherInfo.desc}</h4>
       </div>
@@ -137,4 +66,8 @@ export default function CurrentWeather(props) {
       </div>
     </div>
   );
+  // } else {
+  //   // search();
+  //   return null;
+  // }
 }
--- fallen-eagle/src/DataSearch.js
+++ fallen-eagle/src/DataSearch.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CurrentWeather from "./CurrentWeather";
-import Forecast from "./Forecast";
+// import CurrentWeather from "./CurrentWeather";
+// import Forecast from "./Forecast";
 
 import "./styles/DataSearch.css";
 
 export default function DataSearch(props) {
-  const [query, setQuery] = useState(props.defaultCity);
+  const [input, setInput] = useState("");
   const [weatherData, setWeatherData] = useState({ searched: false });
 
-  useEffect(() => {
-    search(query);
-  }, []);
+  const fetchWeatherData = async (city) => {
+    const apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
+    const units = "metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 
-  function displayTemp(response) {
+    const response = await axios.get(apiUrl);
     setWeatherData({
       searched: true,
       date: response.data.time,
@@ -28,24 +29,19 @@ export default function DataSearch(props) {
         alt: response.data.condition.icon,
       },
     });
-  }
+  };
 
-  async function search() {
-    const apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
-    const units = "metric";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=${units}`;
-    const response = await axios.get(apiUrl);
-    displayTemp(response);
-  }
+  useEffect(() => {
+    fetchWeatherData(props.defaultCity);
+  }, [props.defaultCity]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    search();
+    fetchWeatherData(input);
   }
 
-  function handleQuery(event) {
-    console.log(event.target.value);
-    setQuery(event.target.value);
+  function handleInputChange(event) {
+    setInput(event.target.value);
   }
 
   let form = (
@@ -58,7 +54,7 @@ export default function DataSearch(props) {
               className="form-control"
               placeholder="Enter a city"
               autoFocus={true}
-              onChange={handleQuery}
+              onChange={handleInputChange}
             />
           </div>
           <div className="col-3">
@@ -69,16 +65,37 @@ export default function DataSearch(props) {
     </div>
   );
 
-  if (weatherData.searched) {
-    return (
-      <div className="DataSearch">
-        <div>{form}</div>
-        <CurrentWeather weatherInfo={weatherData} />
-        <Forecast coords={weatherData.coords} icon={weatherData.icon} />
+  // if (weatherData.searched) {
+  return (
+    <div className="DataSearch">
+      <div>{form}</div>
+      <div className="row CurrentWeather">
+        <div className="col-6">
+          <h1>{weatherData.city}</h1>
+          <h6>{weatherData.date}</h6>
+        </div>
+        <div className="col-6 align-self-center">
+          {/* <Temperature
+              metric={props.weatherInfo.temp}
+              icon={props.weatherInfo.icon}
+            /> */}
+          <h4 className="text-capitalize">{weatherData.desc}</h4>
+          <ul>
+            <li>
+              <span className="info-text">Humidity:</span> {weatherData.hum}%
+            </li>
+            <li>
+              <span className="info-text">Wind:</span> {weatherData.wind} m/s
+            </li>
+          </ul>
+        </div>
       </div>
-    );
-  } else {
-    // search(query);
-    return <div>Loading...</div>;
-  }
+      {/* <CurrentWeather weatherInfo={weatherData} /> */}
+      {/* <Forecast coords={weatherData.coords} icon={weatherData.icon} /> */}
+    </div>
+  );
+  // } else {
+  //   // search(query);
+  //   return <div>Loading...</div>;
+  // }
 }
